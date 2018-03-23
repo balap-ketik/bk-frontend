e3ef386984afd6e42c619ef18f35a25855bbcc60
haripermadi <harinurpermadi@gmail.com>
2018-03-23 15:46:02 +0700

    modal challange

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div class="container"> -->
     <div class="leaderboard">
-      <h2><i class="fas fa-trophy"></i> Leader Board Scores</h2>
+      <h2><i class="fas fa-trophy"></i><span> Leader Board Scores</span></h2>
       <div class="tablescore">
         <div class="table-title">
           <div class="thead">
@@ -69,26 +69,42 @@ export default {
   margin-right: auto; */
   padding: 5px;
   background-color: whitesmoke;
+  border: 10px solid transparent;
+  /* padding: 15px; */
+  -webkit-border-image: url(https://www.w3schools.com/cssref/border.png) 30 round;
+  -o-border-image: url(https://www.w3schools.com/cssref/border.png) 30 round;
+  border-image: url(https://www.w3schools.com/cssref/border.png) 30 round;
 }
 .table-title
 {
   border-bottom: 1px solid black;
   background-color: rgb(253, 185, 11);
   align-items: center;
+  font-weight: bold;
+  padding-bottom: 10px;
+  padding-top:  10px;
+  padding-left: 5px;
+  padding-right: 5px;
   /* margin-left: 10%; */
   /* margin-right: 10%; */
 }
 .table-title
 {
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: 45px auto 80px;
 }
 .table-body
 {
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: 45px auto 80px;
+  padding: 5px;
 }
 .fa-trophy{
   color: gold;
+  text-shadow: 2px 2px 2px goldenrod;
 }
+span {
+  text-shadow: 2px 2px 2px grey;
+}
+
 </style>
