a0d122aaa17b9947257accd90412d665895aced5
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-23 16:29:19 +0700

    Logout function

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,33 @@
 <template>
-<div id="dashboard">
-  <div>
-    <jumbo></jumbo>
-  </div>
-  <div class="container" id="home">
-    <div class="players">
-      <h1>Player List</h1>
-      <div class="board" v-if="players.length">
-        <div class="boardpack" v-for="(player, index) in players" :key=index>
-          <div class="playerlist">
-            <h3>{{player}}</h3>
-          </div>
-          <div class="logo">
-            <notif :player='player'></notif>
+<div>
+  <div id="dashboard">
+    <div>
+      <jumbo></jumbo>
+    </div>
+    <div class="container" id="home">
+      <div class="players">
+        <h1>Player List</h1>
+        <div class="board" v-if="players.length">
+          <div class="boardpack" v-for="(player, index) in players" :key=index>
+            <div class="playerlist">
+              <h3>{{player}}</h3>
+            </div>
+            <div class="logo">
+              <notif :player='player'></notif>
+            </div>
           </div>
         </div>
+        <div class="board" v-else>
+          <h3>No online player available</h3>
+        </div>
       </div>
-      <div class="board" v-else>
-        <h3>No online player available</h3>
+      <div class="leaderboard">
+        <leaderboard></leaderboard>
       </div>
     </div>
-    <div class="leaderboard">
-      <leaderboard></leaderboard>
-    </div>
+  </div>
+  <div>
+    <button class="button" @click="logout">Logout</button>
   </div>
 </div>
 </template>
@@ -76,6 +81,10 @@ export default {
       for (var i = 0; i < 5; i++) {
         this.player += possible.charAt(Math.floor(Math.random() * possible.length))
       }
+    },
+    logout () {
+      localStorage.clear()
+      this.$router.push('/')
     }
   }
 
@@ -83,6 +92,37 @@ export default {
 </script>
 
 <style>
+
+.button {
+  display: inline-block;
+  padding: 15px;
+  margin-right: 5px;
+  height: 15px;
+  min-width: 80px;
+  background: #348AA7;
+  border: none;
+  outline: none;
+  color: white;
+  font-family: inherit;
+  font-weight: 400;
+  font-size: 15px;
+  border-radius: 3px;
+  box-shadow: 0 5px 0px #348aa7;
+  border-bottom: 2px solid #30809b;
+  margin-top: 3em;
+}
+.button:hover {
+  background: #2e7a94;
+  box-shadow: 0 4px 1px #2e7a94;
+  border-bottom: 2px solid #2a7088;
+  transition: all 0.3s ease-in;
+}
+.button:active{
+  transform: translateY(4px);
+  border-bottom-width: 2px;
+  box-shadow: none;
+}
+
 #dashboard
 {
   display: flex;
